3d3769cbeb908ab4a36b2e42ea61e31c2fac8347
JIPSON-EXODIEL\jipso <jipson09saad@gmail.com>
2019-06-09 22:48:31 -0500

    módulo categorías terminado

---
+++ resources/views/contenido/contenido.blade.php
@@ -0,0 +1,34 @@
+@extends('principal')
+@section('contenido')
+<template v-if="menu==0">
+   <h1>Contenido del menu 0</h1>
+</template>
+<template v-if="menu==1">
+    <categoria></categoria>
+</template>
+<template v-if="menu==2">
+    <h1>Contenido del menu 2</h1>
+</template>
+<template v-if="menu==3">
+    <h1>Contenido del menu 3</h1>
+</template>
+<template v-if="menu==4">
+    <h1>Contenido del menu 4</h1>
+</template>
+<template v-if="menu==5">
+    <h1>Contenido del menu 5</h1>
+</template>
+<template v-if="menu==6">
+    <h1>Contenido del menu 6</h1>
+</template>
+<template v-if="menu==7">
+    <h1>Contenido del menu 7</h1>
+</template>
+<template v-if="menu==8">
+    <h1>Contenido del menu 8</h1>
+</template>
+<template v-if="menu==9">
+    <h1>Contenido del menu 9</h1>
+</template>
+
+@endsection
